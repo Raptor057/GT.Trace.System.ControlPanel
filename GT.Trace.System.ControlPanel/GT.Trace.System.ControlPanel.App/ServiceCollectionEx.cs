8e6c0e2b5e86e5e1adca1dd9f1b1b2559dc43cc7
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-06-13 18:47:20 -0500

    Project ready to start

---
+++ GT.Trace.System.ControlPanel/GT.Trace.System.ControlPanel.App/ServiceCollectionEx.cs
@@ -0,0 +1,16 @@
+﻿using Common.Common.CleanArch;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GT.Trace.System.ControlPanel.App
+{
+    public static class ServiceCollectionEx
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services)
+        {
+            return services
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(InteractorPipeline<,>))
+                .AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(ServiceCollectionEx).Assembly); });
+        }
+    }
+}
---
+++ GT.Trace.System.ControlPanel/GT.Trace.System.ControlPanel.WebApi/ServiceCollectionEx.cs
@@ -0,0 +1,24 @@
+﻿using Common.Common.CleanArch;
+using GT.Trace.System.ControlPanel.App;
+using GT.Trace.System.ControlPanel.Infra;
+
+namespace GT.Trace.System.ControlPanel.WebApi
+{
+    public static class ServiceCollectionEx
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", false, true)
+                    .Build();
+
+            return services
+                .AddSingleton(config)
+                .AddInfraServices(config)
+                .AddSingleton(typeof(ResultViewModel<>))
+                .AddSingleton(typeof(GenericViewModel<>))
+                .AddAppServices()
+                .AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(ServiceCollectionEx).Assembly); });
+        }
+    }
+}
